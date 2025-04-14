46bdd0999dc81748056a5821fe036a60010f21c1
Aytac Agazade <aytacagazade1603@gmail.com>
2025-04-14 12:03:27 +0400

    feat: Extend Customer class with PremiumCustomer supporting discount rate

---
+++ src/ecommerceApplication/PremiumCustomer.java
@@ -0,0 +1,18 @@
+package ecommerceApplication;
+
+public class PremiumCustomer extends Customer{
+    private double discountRate;
+
+    public PremiumCustomer(int id, String name, String email, double discountRate) {
+        super(id, name, email);
+        this.discountRate = discountRate;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public double getDiscountedPrice(double price) {
+        return price - (price * discountRate);
+    }
+}
