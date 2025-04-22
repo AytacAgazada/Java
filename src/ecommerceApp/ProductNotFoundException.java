3ad88fd5412dabbf7222ca397ebd34e7b5b7d1a1
Aytac Agazade <aytacagazade1603@gmail.com>
2025-04-22 09:09:25 +0400

    feat: add custom exception ProductNotFoundException for missing product handling

---
+++ src/ecommerceApp/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package ecommerceApp;
+
+public class ProductNotFoundException extends RuntimeException{
+
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
