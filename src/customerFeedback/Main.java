49c6fee90c2ef6e7c62fa2f7404957cbcc1ee12a
Aytac Agazade <aytacagazade1603@gmail.com>
2025-05-07 07:25:20 +0400

    Main

---
+++ src/customerFeedback/Main.java
@@ -0,0 +1,9 @@
+package customerFeedback;
+
+public class Main {
+    public static void main(String[] args) {
+        FeedbackProcessor.loadRawFeedback();
+        FeedbackProcessor.processFeedback();
+        FeedbackProcessor.showMenu();
+    }
+}
